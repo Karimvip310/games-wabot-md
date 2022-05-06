1c215835326c38e68c4ffc0c5d1a5bf3229b73e2
Karimvip310 <103865083+Karimvip310@users.noreply.github.com>
2022-05-06 14:47:04 +0000

    Add files via upload

---
+++ plugins/owner-savefile.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+
+let handler = async (m, { text, usedPrefix, command }) => {
+	if (!text) throw `Where is the path?\n\nEx:\n${usedPrefix + command} plugins/menu.js`
+	if (!m.quoted?.text) throw 'Reply code' 
+	await fs.writeFileSync(text, m.quoted.text)
+	m.reply(`Saved ${text} to file!`)
+}
+handler.help = ['savefile', 'sf'].map(v => v + ' <path>')
+handler.tags = ['owner']
+handler.command = ['savefile', 'sf']
+handler.rowner = true
+
+export default handler
